74a5ac41f467144afb221dd558014ed34d3d9e20
Briuor <briuorlol89@gmail.com>
2022-12-26 08:47:39 -0300

    configuring to deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/brunolopes.dev/' : '',
   images: {
     domains: ['github.com'],
-},
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
