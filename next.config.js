4aa6fc7f7065318fda6e3047f7935ae245a867e2
Briuor <briuorlol89@gmail.com>
2022-12-26 09:31:19 -0300

    fixing deploy

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/brunolopes.dev/' : '',
+  assetPrefix: isProd ? './' : '',
   images: {
     domains: ['github.com'],
     unoptimized: true,
