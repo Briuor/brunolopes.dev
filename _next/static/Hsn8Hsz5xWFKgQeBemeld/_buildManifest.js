2da9b602b555b3db2a1fdf86fcfc134c98955cdd
Briuor <Briuor@users.noreply.github.com>
2022-12-26 15:48:28 +0000

    deploy: dc9ec6e628c5d4743fd6b82ba32c7bd8ba3b44c8

---
+++ _next/static/Hsn8Hsz5xWFKgQeBemeld/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/05b82f4c6484ad01.css","static/chunks/pages/index-737253ff9b13f597.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
